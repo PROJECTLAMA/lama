09ca0e159d9e7fabc628f31f3b4dba060ee5bf12
romeoh <brianpaek@Brianui-MacBook-Pro.local>
2015-10-02 14:22:58 +0900

    라이브러리 설정

---
+++ api/dbcon.php
@@ -0,0 +1,22 @@
+<?php
+//$conn=mysql_connect('localhost', 'gaeyoucom', 'love7878');
+//$db=mysql_select_db("gaeyoucom", $conn);
+$setting = array(// required
+	'database_type' => 'mysql',
+	'database_name' => 'gaeyoucom',
+	'server' => 'localhost',
+	'username' => 'gaeyoucom',
+	'password' => 'love7878',
+	'charset' => 'utf8',
+ 
+	// optional
+	'port' => 3306,
+	// driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+	'option' => array(
+		PDO::ATTR_CASE => PDO::CASE_NATURAL
+	)
+);
+$database = new medoo($setting);
+?>
+
+
